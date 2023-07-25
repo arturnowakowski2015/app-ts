6484563094ac9db649bd9fec4c09f0ee08044a99
artur nowakowski <63631140+arturnowakowski2015@users.noreply.github.com>
2023-07-25 19:45:22 +0000

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -42,7 +42,7 @@ function App() {
   const [datalength, setDatalength] = useState<number>(0);
 
   const [set, setSet] = useState<Set>({
-    host: "https://3chphk-3001.csb.app/",
+    host: "https://3k35kg-3001.csb.app/",
     actcategory: "new",
     database: "comments",
   });
