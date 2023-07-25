4e4f30000ea554b7fa61ea2b5c595959180b9ea2
artur nowakowski <63631140+arturnowakowski2015@users.noreply.github.com>
2023-07-25 20:00:57 +0000

    Update useGetTableData.tsx

--- client/src/features/layout/table/useGetTableData.tsx
+++ client/src/features/layout/table/useGetTableData.tsx
@@ -43,7 +43,7 @@ const useGetPaginatedData = (
         currentPage +
         "/" +
         10;
-      alert(url);
+      alert(url + ":::");
       let t: any = await axios.get(url);
       //await getRec(url);
       alert(url + "ddd " + JSON.stringify(t));
