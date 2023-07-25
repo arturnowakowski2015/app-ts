712f82b48415adcc9a03d634dfcc76a69df64a15
arturnowakowski2015 <63631140+arturnowakowski2015@users.noreply.github.com>
2023-07-25 17:17:58 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+/*
+
 import express from "express";
 import http from "http";
 import { fileURLToPath } from "url";
@@ -35,4 +37,27 @@ const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
 export default app;
+*/
+
+import express from 'express'
+import { Router } from 'express'
+
+const app = express()
+
+const route = Router()
+
+route.get('/', (req, res) => {
+    return res.json({
+        success: true,
+        message: "Sucesso!"
+    })
+})
+
+app.use(route)
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+    console.log('Server running in ' + port)
+})
 // next line is the money
