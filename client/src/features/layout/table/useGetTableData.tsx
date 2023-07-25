c85e4695e872584e520fabf4e7d13660eb8bb1f5
artur nowakowski <63631140+arturnowakowski2015@users.noreply.github.com>
2023-07-25 18:39:04 +0000

    Update useGetTableData.tsx

--- client/src/features/layout/table/useGetTableData.tsx
+++ client/src/features/layout/table/useGetTableData.tsx
@@ -43,11 +43,11 @@ const useGetPaginatedData = (
         currentPage +
         "/" +
         10;
-
+      alert("ddd ");
       let t: any = await getRec(
         "https://app-ts-five.vercel.app/comments/paginate/new/1/10"
       );
-      alert("ddd ");
+
       return t;
     },
     { keepPreviousData: true, staleTime: 10000000000000 }
