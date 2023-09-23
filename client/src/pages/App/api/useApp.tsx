1198a6a3d26ada0d21ea792be811e44ed7682618
artur nowakowski <63631140+arturnowakowski2015@users.noreply.github.com>
2023-09-23 05:41:00 +0000

    Update useUpdateRec.tsx and useApp.tsx

--- client/src/pages/App/api/useApp.tsx
+++ client/src/pages/App/api/useApp.tsx
@@ -27,7 +27,8 @@ const useApp = () => {
   const [datalength, setDatalength] = useState<number>(0);
 
   const [set1, setSet1] = useState<Set>({
-    host: "http://localhost:3001/",
+    host: "/comments/new/update/",
+    //"http://localhost:3001/",
     actcategory: "new",
     database: "comments",
   });
