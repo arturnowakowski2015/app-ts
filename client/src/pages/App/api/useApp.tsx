c64bb3fc40066c688de01de2e5656957ebd3a4dd
artur nowakowski <63631140+arturnowakowski2015@users.noreply.github.com>
2023-09-23 06:25:55 +0000

    Update useApp.tsx

--- client/src/pages/App/api/useApp.tsx
+++ client/src/pages/App/api/useApp.tsx
@@ -27,7 +27,7 @@ const useApp = () => {
   const [datalength, setDatalength] = useState<number>(0);
 
   const [set1, setSet1] = useState<Set>({
-    host: "//rk7jxt-3000.csb.app/",
+    host: "//https://rk7jxt-3000.csb.app/",
     //"http://localhost:3001/",
     actcategory: "new",
     database: "comments",
