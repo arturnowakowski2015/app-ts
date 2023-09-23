9d511c889a65602c10691080942efe67d22c75e7
artur nowakowski <63631140+arturnowakowski2015@users.noreply.github.com>
2023-09-23 06:22:01 +0000

    Update useApp.tsx

--- client/src/pages/App/api/useApp.tsx
+++ client/src/pages/App/api/useApp.tsx
@@ -27,7 +27,7 @@ const useApp = () => {
   const [datalength, setDatalength] = useState<number>(0);
 
   const [set1, setSet1] = useState<Set>({
-    host: "//xydxrz-3000.csb.app/",
+    host: "///",
     //"http://localhost:3001/",
     actcategory: "new",
     database: "comments",
