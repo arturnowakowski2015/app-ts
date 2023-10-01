685eec13304004b411624960eb831151e29477ca
artur nowakowski <63631140+arturnowakowski2015@users.noreply.github.com>
2023-10-01 02:55:52 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,14 +22,18 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(express.json());
- const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions)) 
- 
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      "http://localhost:3001",
+      "http://localhost:3000",
+      "http://127.0.0.1:3002",
+      "https://xydxrz-3000.csb.app"
+    ],
+  })
+);
 increase();
 app.get(
   "/:database/sort/:actcategory/:column/:sortDirection/:page/:limit",
