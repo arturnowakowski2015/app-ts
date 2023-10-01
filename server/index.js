b7c4b3f70280542f72ed161d0b2385ab58b406b3
artur nowakowski <63631140+arturnowakowski2015@users.noreply.github.com>
2023-10-01 04:11:56 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,23 +18,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 const server = http.createServer(app);
 
 app.use(express.json());
-
 app.use(
   cors({
-    'Access-Control-Allow-Credentials':true,    origin: [
-      "http://localhost:3001",
-      "http://localhost:3000",
-      "http://127.0.0.1:3002",
-      "https://xydxrz-3000.csb.app"
-    ],
+    credentials: true,
+    origin: true,
   })
 );
 increase();
