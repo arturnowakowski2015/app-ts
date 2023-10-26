b55e2df5164e98c73c7517f4b0a348a55544d20a
artur nowakowski <63631140+arturnowakowski2015@users.noreply.github.com>
2023-10-26 00:30:24 +0000

    Update useUpdateRec.tsx and useApp.tsx

--- client/src/pages/App/api/useApp.tsx
+++ client/src/pages/App/api/useApp.tsx
@@ -27,7 +27,7 @@ const useApp = () => {
   const [datalength, setDatalength] = useState<number>(0);
 
   const [set1, setSet1] = useState<Set>({
-    host: "http://localhost:3001/",
+    host: "//nfw8h4-3001.csb.app/",
     actcategory: "new",
     database: "comments",
   });
